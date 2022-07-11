1e5ed9a9a62bb437bb26192fac9e6f5ff3a297d4
Essoh Jean Penuel <penuel225@gmail.com>
2022-07-11 21:01:09 +0000

    sdfgh

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const dbConnexion = new Client({
-    host: "ec2-34-247-172-149.eu-west-1.compute.amazonaws.com",
-    user: "trivbutsqxtilm",
-    database: 'todo_database',
-    password: '126c2513f28e7e7b42719858f9108842248e817c448c0176febe39607c6c0b09',
+    host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
+    user: "lzdntrcnigsqge",
+    database: 'dbruko73j9tpvu',
+    password: 'd81f03d43ea50e9017be095fef315f32d182ee28172da5e924f6d63a1d7894f4',
     port: 5432
 });
 
+// 
 module.exports = dbConnexion;
