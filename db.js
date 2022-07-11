9d0d4fc3ed23d52c564b31af8657215dc2f9bdb8
Essoh Jean Penuel <penuel225@gmail.com>
2022-07-11 15:10:05 +0000

    dfghjk

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const { Client } = require('pg')
 require('dotenv').config()
 
 const dbConnexion = new Client({
-    host: "localhost",
-    user: "postgres",
+    host: "ec2-34-247-172-149.eu-west-1.compute.amazonaws.com",
+    user: "trivbutsqxtilm",
     database: 'todo_database',
-    password: process.env.DATABASE_PASSWORD,
+    password: '126c2513f28e7e7b42719858f9108842248e817c448c0176febe39607c6c0b09',
     port: 5432
 });
 
